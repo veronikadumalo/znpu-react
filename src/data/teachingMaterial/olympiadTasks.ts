bf8bb6c29a05a5afd8d5384a1dbcb6e46e2d6343
veronikadumalo <dumalo44084@gmail.com>
2023-05-10 02:36:51 +0200

    feat(olympiads): add files

---
+++ src/data/teachingMaterial/olympiadTasks.ts
@@ -0,0 +1,276 @@
+import { LinkData } from "../../types/general";
+
+interface OlympiadTask {
+  title: string;
+  years: string;
+  files: LinkData[];
+  keys: Keys;
+}
+interface Keys {
+  files: LinkData[];
+}
+
+export const OLYMPIAD_TASKS: OlympiadTask[] = [
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ МОВИ",
+    years: "2014/2015 р.",
+    files: [
+      {
+        title: "8 клас",
+        link: "",
+      },
+      {
+        title: "9 клас",
+        link: "",
+      },
+      {
+        title: "10 клас",
+        link: "",
+      },
+      {
+        title: "11 клас",
+        link: "",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "8 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-8.docx",
+        },
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-9.docx",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-10.docx",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-11.docx",
+        },
+      ],
+    },
+  },
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ ЛІТЕРАТУРИ",
+    years: "2014/2015 р.",
+    files: [
+      {
+        title: "8 клас",
+        link: "",
+      },
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/olimpiada-9-klasa.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/olimpiada-10-klasa.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/olimpiada-11-klasa.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "8 клас",
+          link: "",
+        },
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/KLUCZ-9-KLASA.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/KLUCZ-10-klasa.doc",
+        },
+        {
+          title: "11 клас",
+          link: "",
+        },
+      ],
+    },
+  },
+  {
+    title: "РАЙОННА ОЛІМПІАДА З ПОЛЬСЬКОЇ МОВИ",
+    years: "2015 рік",
+    files: [
+      {
+        title: "8 клас",
+        link: "/files/teachingMaterial/olympiadTasks/8_klasa_A.doc",
+      },
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/9_klasa_A.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/10_klasa_A.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/11_klasa_A.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "8 клас",
+          link: "/files/teachingMaterial/olympiadTasks/Tekst_ i_klucz_8_klasa_A.doc",
+        },
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/Tekst_ i_klucz_9_klasa_A.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/Tekst_ i_klucz_10_klasa_A.doc",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/Tekst_ i_klucz_11_klasa_A.doc",
+        },
+      ],
+    },
+  },
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ МОВИ",
+    years: "2016/2017 р.",
+    files: [
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/9-KLASA.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/10-klasa.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/11-klasa.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/9-klasa-KLUCZ-I-TEKST.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/10-klasa-KLUCZ-I-TEKST.doc",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/11-klasa-KLUCZ-I-TEKST.doc",
+        },
+      ],
+    },
+  },
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ ЛІТЕРАТУРИ",
+    years: "2016/2017 р.",
+    files: [
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/9-klasaL.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/10-klasaL.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/11-klasaL.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-9.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-10.doc",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz-11.doc",
+        },
+      ],
+    },
+  },
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ МОВИ",
+    years: "2018/2019 р.",
+    files: [
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/KLASA_9.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/KLASA_10.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/KLASA_11.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/KLUCZ_kl.9.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/KLUCZ_kl.10.docx.doc",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/KLUCZ_kl.11.doc",
+        },
+      ],
+    },
+  },
+  {
+    title: "ВСЕУКРАЇНСЬКА ОЛІМПІАДА З ПОЛЬСЬКОЇ ЛІТЕРАТУРИ",
+    years: "2018/2019 р.",
+    files: [
+      {
+        title: "9 клас",
+        link: "/files/teachingMaterial/olympiadTasks/literatura_9_klasa2019.doc",
+      },
+      {
+        title: "10 клас",
+        link: "/files/teachingMaterial/olympiadTasks/literatura_10_klasa2019.doc",
+      },
+      {
+        title: "11 клас",
+        link: "/files/teachingMaterial/olympiadTasks/literatura_11_klasa2019.doc",
+      },
+    ],
+    keys: {
+      files: [
+        {
+          title: "9 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz_9_klasa2019.doc",
+        },
+        {
+          title: "10 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz_10_klasa2019.doc",
+        },
+        {
+          title: "11 клас",
+          link: "/files/teachingMaterial/olympiadTasks/klucz_11_klasa2019.doc",
+        },
+      ],
+    },
+  },
+];
