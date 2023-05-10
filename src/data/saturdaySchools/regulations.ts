7884bec6abfcb915c5b53c984f9cc8657d17f49f
veronikadumalo <dumalo44084@gmail.com>
2023-05-10 18:58:46 +0200

    feat(regulation): add content

---
+++ src/data/saturdaySchools/regulations.ts
@@ -0,0 +1,9 @@
+import { LinkData } from "../../types/general";
+
+export const REGULATIONS: LinkData[] = [
+  {
+    title:
+      "ТИПОВЕ ПОЛОЖЕННЯ про культурно-освітній центр національних меншин України",
+    link: "/files/saturdaySchools/regulations/ТИПОВЕ-ПОЛОЖЕННЯ.doc",
+  },
+];
