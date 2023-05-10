f0e06c5b85a5d530a4000d1469387e4511168e70
veronikadumalo <dumalo44084@gmail.com>
2023-05-10 18:48:40 +0200

    feat(ScenariosOfCelebrations): add content

---
+++ src/data/teachingMaterial/scenariosOfCelebrations.ts
@@ -0,0 +1,8 @@
+import { LinkData } from "../../types/general";
+
+export const SCENARIOS_OF_CELEBRATIONS: LinkData[] = [
+  {
+    title: "Scenariusze Uroczystosci Szkolnych",
+    link: "/files/teachingMaterial/scenariosOfCelebrations/Scenariusze-Uroczystosci-Szkolnych.pdf",
+  },
+];
--- src/views/ScenariosOfCelebrations.tsx
+++ src/views/ScenariosOfCelebrations.tsx
@@ -1,11 +1,39 @@
+import styled from "styled-components/macro";
 import Layout from "../components/Layout";
 import { SubMenu } from "../components/SubMenu";
 import { NAVIGATION } from "../data/navigation";
+import { SCENARIOS_OF_CELEBRATIONS } from "../data/teachingMaterial/scenariosOfCelebrations";
+import FileLink from "../components/FileLink";
+
+const StyledContainer = styled.div`
+  display: flex;
+`;
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 20px;
+  width: 100%;
+  @media (max-width: 700px) {
+    padding: 0;
+  }
+`;
 
 const ScenariosOfCelebrations = () => {
   return (
     <Layout>
-      <SubMenu submenuItems={NAVIGATION[5].subpages} />
+      <StyledContainer>
+        <SubMenu submenuItems={NAVIGATION[5].subpages} />
+        <StyledContent>
+          {SCENARIOS_OF_CELEBRATIONS.map((book) => (
+            <FileLink
+              key={book.title}
+              fileLink={book.link}
+              title={book.title}
+            />
+          ))}
+        </StyledContent>
+      </StyledContainer>
     </Layout>
   );
 };
