f156085b29bc8fc75e0c9556d5752e42ffb7633b
veronikadumalo <dumalo44084@gmail.com>
2023-05-10 19:08:58 +0200

    feat(programs): add content

---
+++ src/data/saturdaySchools/programs.ts
@@ -0,0 +1,32 @@
+import { LinkData } from "../../types/general";
+
+export const PROGRAMS: LinkData[] = [
+  {
+    title: "Історія",
+    link: "/files/saturdaySchools/programs/Historia.doc",
+  },
+  {
+    title: "Польська література",
+    link: "/files/saturdaySchools/programs/literatur-pol.doc",
+  },
+  {
+    title: "Музика",
+    link: "/files/saturdaySchools/programs/muzyka-program.doc",
+  },
+  {
+    title: "Народознавство",
+    link: "/files/saturdaySchools/programs/Narodoznavstvo.doc",
+  },
+  {
+    title: "Польська мова",
+    link: "/files/saturdaySchools/programs/Польська-мова.docx",
+  },
+  {
+    title: "Польська мова та культура",
+    link: "/files/saturdaySchools/programs/programay-z-pol-movy-ta-kultury.doc",
+  },
+  {
+    title: "Збірник програм для суботніх шкіл",
+    link: "/files/saturdaySchools/programs/Zbirnyk-program-dla-sobotnich-szkol.pdf",
+  },
+];
