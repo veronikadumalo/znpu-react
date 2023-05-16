2329c912bf15ea0e341f8d7c5b7aa38691c285e6
veronikadumalo <dumalo44084@gmail.com>
2023-05-16 18:26:33 +0200

    feat(input): create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components/macro";
+
+interface InputProps {
+  label?: string;
+  inputId: string;
+  inputType?: string;
+}
+
+const StyledInputContainer = styled.div``;
+const StyledLabel = styled.label``;
+const StyledInput = styled.input``;
+
+export const Input = ({ label, inputId, inputType = "text" }: InputProps) => {
+  return (
+    <StyledInputContainer>
+      <StyledLabel>{label}</StyledLabel>
+      <StyledInput id={inputId} type={inputType} />
+    </StyledInputContainer>
+  );
+};
