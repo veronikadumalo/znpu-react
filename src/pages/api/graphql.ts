39e2b70f63cb8e28257232a5d9ee2321c04d03e4
veronikadumalo <dumalo44084@gmail.com>
2023-10-22 23:59:41 +0200

    refactor(graphql): update

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,39 +1,19 @@
-// import { createYoga } from "graphql-yoga";
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { schema } from "../../../graphql/schema";
+import { createYoga } from "graphql-yoga";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { schema } from "../../../graphql/schema";
 
-// export default createYoga<{
-//   req: NextApiRequest;
-//   res: NextApiResponse;
-// }>({
-//   schema,
-//   graphqlEndpoint: "/api/graphql",
-// });
+const yoga = createYoga<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  schema,
+  graphqlEndpoint: "/api/graphql",
+});
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-import { ApolloServer } from "@apollo/server";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { gql } from "graphql-tag";
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "Hello World",
+export const config = {
+  api: {
+    bodyParser: false,
   },
 };
 
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-export default startServerAndCreateNextHandler(apolloServer);
+export { yoga as GET, yoga as POST };
