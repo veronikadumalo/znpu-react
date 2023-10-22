c48d03d75058e7b2dd6f6b44e6e62c7d3a0f425e
veronikadumalo <dumalo44084@gmail.com>
2023-10-23 00:06:51 +0200

    fix(graphql): fix bug

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,12 +2,16 @@ import { createYoga } from "graphql-yoga";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { schema } from "../../../graphql/schema";
 
-const yoga = createYoga<{
+const { handleRequest } = createYoga<{
   req: NextApiRequest;
   res: NextApiResponse;
 }>({
   schema,
   graphqlEndpoint: "/api/graphql",
+  fetchAPI: {
+    Response: Response,
+    Request: Request,
+  },
 });
 
 export const config = {
@@ -16,4 +20,4 @@ export const config = {
   },
 };
 
-export { yoga as GET, yoga as POST };
+export { handleRequest as GET, handleRequest as POST };
