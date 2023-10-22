95e902462a9a6c89d1a2d0b83e6d83785f021cba
veronikadumalo <dumalo44084@gmail.com>
2023-10-22 23:19:42 +0200

    refactor(apollo): install apollo server

--- src/graphql/query/departaments.ts
+++ src/graphql/query/departaments.ts
@@ -2,14 +2,6 @@ import { gql } from "@apollo/client";
 
 export const DEPARTAMENTS_QUERY = gql`
   query Deparments {
-    deparments {
-      id
-      title
-      persons {
-        id
-        name
-        email
-      }
-    }
+    hello
   }
 `;
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,17 +1,39 @@
-import { createYoga } from "graphql-yoga";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { schema } from "../../../graphql/schema";
+// import { createYoga } from "graphql-yoga";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { schema } from "../../../graphql/schema";
 
-export default createYoga<{
-  req: NextApiRequest;
-  res: NextApiResponse;
-}>({
-  schema,
-  graphqlEndpoint: "/api/graphql",
-});
+// export default createYoga<{
+//   req: NextApiRequest;
+//   res: NextApiResponse;
+// }>({
+//   schema,
+//   graphqlEndpoint: "/api/graphql",
+// });
 
-export const config = {
-  api: {
-    bodyParser: false,
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World",
   },
 };
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+export default startServerAndCreateNextHandler(apolloServer);
