acf51017c2ab762f7f56751c8da922ac3a9a703b
veronikadumalo <dumalo44084@gmail.com>
2023-10-25 21:11:46 +0200

    refactor(graphql): update url

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,8 +1,17 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { createNetworkStatusNotifier } from "react-apollo-network-status";
+
+const { link } = createNetworkStatusNotifier();
 
 const apolloClient = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
+  link: link.concat(
+    createHttpLink({
+      uri: "https://graphql-api-ozx5.onrender.com/graphql",
+    })
+  ),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 });
 
 export default apolloClient;
