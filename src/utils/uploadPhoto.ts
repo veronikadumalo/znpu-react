18d01147478014fb447c88be5feb29c0df71f1c0
veronikadumalo <dumalo44084@gmail.com>
2023-10-31 18:59:54 +0100

    refactor(upload file): update

--- src/utils/uploadPhoto.ts
+++ src/utils/uploadPhoto.ts
@@ -1,16 +1,41 @@
+import aws from "aws-sdk";
 export const uploadPhoto = async (file?: File) => {
   if (!file) return null;
   const filename = encodeURIComponent(file.name);
-  const res = await fetch(`/api/upload-image?file=${filename}`);
-  const data = await res.json();
+  const s3 = new aws.S3({
+    accessKeyId: "AKIAVAGLBMHQSDODUESZ",
+    secretAccessKey: "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",
+    region: "eu-central-1",
+  });
+  aws.config.update({
+    accessKeyId: "AKIAVAGLBMHQSDODUESZ",
+    secretAccessKey: "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",
+    region: "eu-central-1",
+    signatureVersion: "v4",
+  });
+
+  const post = await s3.createPresignedPost({
+    Bucket: "znpu-bucket",
+    Fields: {
+      key: filename,
+    },
+    Expires: 60, // seconds
+    Conditions: [
+      ["content-length-range", 0, 5048576], // up to 1 MB
+    ],
+  });
+  console.log(post);
+
+  // const res = await fetch(`/api/upload-image?file=${filename}`);
+  // const data = await res.json();
   const formData = new FormData();
 
-  Object.entries({ ...data.fields, file }).forEach(([key, value]) => {
+  Object.entries({ ...post.fields, file }).forEach(([key, value]) => {
     // @ts-ignore
     formData.append(key, value);
   });
   try {
-    const fileRes = await fetch(data.url, {
+    const fileRes = await fetch(post.url, {
       method: "POST",
       body: formData,
     });
